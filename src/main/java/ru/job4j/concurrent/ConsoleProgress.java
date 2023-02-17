39e9681d5d9c5e71747c08bba321b2476c78817b
mypc <drinkinswo@gmail.com>
2023-02-17 16:12:37 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        int count = 0;
+        var process = new char[]{'-', '\\', '|', '/'};
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + process[count % process.length]);
+            count++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
+
