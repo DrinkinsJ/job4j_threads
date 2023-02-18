9a04b212ec0ec7eac20d4a6a8ab21ae9a622eb6d
mypc <drinkinswo@gmail.com>
2023-02-18 23:45:35 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -0,0 +1,33 @@
+package ru.job4j.ref;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        for (User user : users.values()){
+            User u = User.of(user.getName());
+            u.setId(user.getId());
+            list.add(u);
+        }
+        return list;
+    }
+}
