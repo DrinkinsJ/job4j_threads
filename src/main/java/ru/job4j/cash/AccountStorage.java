e531660ff7fd9f6dda154718e1e7c5695acebd33
mypc <drinkinswo@gmail.com>
2023-02-21 16:35:33 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -0,0 +1,40 @@
+package ru.job4j.cash;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+@ThreadSafe
+public class AccountStorage {
+    @GuardedBy("this")
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) == null;
+    }
+
+    public synchronized boolean update(Account account) {
+        return accounts.put(account.id(), account) != null;
+    }
+
+    public synchronized boolean delete(int id) {
+        return accounts.remove(id) != null;
+    }
+
+    public synchronized Optional<Account> getById(int id) {
+        return Optional.ofNullable(accounts.get(id));
+    }
+
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
+        Optional<Account> from = getById(fromId);
+        Optional<Account> to = getById(toId);
+        if (from.isPresent() && to.isPresent()) {
+            update(new Account(fromId, from.get().amount() - amount));
+            update(new Account(toId, to.get().amount() + amount));
+            return true;
+        }
+        return false;
+    }
+}
