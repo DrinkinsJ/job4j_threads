5a53f94486591f49be0ddb87348cd553a0e0f586
mypc <drinkinswo@gmail.com>
2023-02-27 18:55:20 +0300

    2. ExecutorService рассылка почты. [#63097 #395733] fix #1

--- src/main/java/ru/job4j/pool/User.java
+++ src/main/java/ru/job4j/pool/User.java
@@ -3,8 +3,8 @@ package ru.job4j.pool;
 import java.util.Objects;
 
 public class User {
-    private String username;
-    private String email;
+    private final String username;
+    private final String email;
 
     public User(String username, String email) {
         this.username = username;
@@ -15,28 +15,7 @@ public class User {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(username, user.username) && Objects.equals(email, user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(username, email);
     }
 }
