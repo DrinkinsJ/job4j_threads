cd534c8ec4bca093d3796ddaf6edbaa530904d4e
mypc <drinkinswo@gmail.com>
2023-03-01 21:16:56 +0300

    3. ForkJoinPool [#315067]

---
+++ src/main/java/ru/job4j/TestExamples.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class TestExamples {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.println(Runtime.getRuntime().freeMemory());
+        System.out.println(Runtime.getRuntime().maxMemory());
+    }
+}
