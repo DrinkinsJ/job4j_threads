87bbec0b5468fdf80ccb4c4a755727b6c209fc70
mypc <drinkinswo@gmail.com>
2023-03-01 14:25:17 +0300

    2. ExecutorService рассылка почты. [#63097 #395733] fix #1

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -4,12 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-
-    private final ExecutorService pool;
-
-    public EmailNotification() {
-        this.pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-    }
+   private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
         String subject = String.format("Notification %s to email %s.", user.getUsername(), user.getEmail());
