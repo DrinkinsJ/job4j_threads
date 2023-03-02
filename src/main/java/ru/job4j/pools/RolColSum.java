19099c9239ea7b484bea4711b247fc3e996c036a
DrinkinsW <drinkinswo@gmail.com>
2023-03-02 22:40:20 +0300

    4. CompletableFuture [#361626 #396879] fix#1

--- src/main/java/ru/job4j/pools/RolColSum.java
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -1,6 +1,5 @@
 package ru.job4j.pools;
 
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -34,40 +33,5 @@ public class RolColSum {
             sums[i] = sumsCompletableFuture(matrix, i).get();
         }
         return sums;
-    }
-
-    public static class Sums {
-        private final int rowSum;
-        private final int colSum;
-
-        public Sums(int rowSum, int colSum) {
-            this.rowSum = rowSum;
-            this.colSum = colSum;
-        }
-
-        @Override
-        public String toString() {
-            return "Sums{"
-                    + "rowSum=" + rowSum
-                    + ", colSum=" + colSum
-                    + '}';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Sums sums = (Sums) o;
-            return rowSum == sums.rowSum && colSum == sums.colSum;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(rowSum, colSum);
-        }
     }
 }
