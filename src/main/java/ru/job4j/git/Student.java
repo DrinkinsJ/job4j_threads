20765ea0a4721570c5977e6948040dd57d5abf5e
mypc <drinkinswo@gmail.com>
2023-03-06 12:59:29 +0300

    3. ForkJoinPool [#315067 #396713] fix#2

---
+++ src/main/java/ru/job4j/git/Student.java
@@ -0,0 +1,11 @@
+package ru.job4j.git;
+
+public class Student {
+    private final int age;
+    private final String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+}
--- src/test/java/ru/job4j/pools/ParallelIndexSearchTest.java
+++ src/test/java/ru/job4j/pools/ParallelIndexSearchTest.java
@@ -2,18 +2,14 @@ package ru.job4j.pools;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ForkJoinPool;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ParallelIndexSearchTest {
 
-    private final ForkJoinPool forkJoinPool = new ForkJoinPool();
-
     @Test
     public void whenSmallArrayFindIndex() {
         Integer[] array = {3, 4, 1};
-        Integer res = forkJoinPool.invoke(new ParallelIndexSearch<>(array, 0, array.length - 1, 4));
+        int res = ParallelIndexSearch.find(array, 4);
         assertThat(res).isEqualTo(1);
     }
 
@@ -23,14 +19,14 @@ public class ParallelIndexSearchTest {
         for (int i = 0; i < 500; i++) {
             array[i] = i;
         }
-        Integer res = forkJoinPool.invoke(new ParallelIndexSearch<>(array, 0, array.length - 1, 333));
+        int res = ParallelIndexSearch.find(array, 333);
         assertThat(res).isEqualTo(333);
     }
 
     @Test
     public void whenSmallArrayFindObj() {
         Object[] array = {"first", "second", "third"};
-        Object res = forkJoinPool.invoke(new ParallelIndexSearch<>(array, 0, array.length - 1, "second"));
+        int res = ParallelIndexSearch.find(array, "second");
         assertThat(res).isEqualTo(1);
     }
 
@@ -40,15 +36,14 @@ public class ParallelIndexSearchTest {
         for (int i = 0; i < 500; i++) {
             array[i] = String.format("Object #%s", i);
         }
-        Object res = forkJoinPool.invoke(new ParallelIndexSearch<>(array, 0, array.length - 1, "Object #333"));
+        int res = ParallelIndexSearch.find(array, "Object #333");
         assertThat(res).isEqualTo(333);
     }
 
     @Test
     public void whenObjNotFound() {
         Object[] array = {"First", "Third", "Fourth"};
-        Object res = forkJoinPool.invoke(new ParallelIndexSearch<>(array, 0, array.length - 1, "Cat"));
+        int res = ParallelIndexSearch.find(array, "Cat");
         assertThat(res).isEqualTo(-1);
     }
-
 }
