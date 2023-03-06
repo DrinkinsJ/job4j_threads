16dfb6c4a0b98d86efb8a6c4053cd2a8edbc8c5b
mypc <drinkinswo@gmail.com>
2023-03-06 18:22:47 +0300

    3.4. Временное хранение. git stash. [#504832] unstash.

--- src/main/java/ru/job4j/git/Student.java
+++ src/main/java/ru/job4j/git/Student.java
@@ -4,8 +4,11 @@ public class Student {
     private final int age;
     private final String name;
 
-    public Student(int age, String name) {
+    private final int course;
+
+    public Student(int age, String name, int course) {
         this.age = age;
         this.name = name;
+        this.course = course;
     }
 }
